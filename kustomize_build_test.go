d6c660c98c3250063276b338c84dcbe5439925b9
趙子賢 <danny900714@gmail.com>
2023-05-31 17:38:40 +0800

    Update test case

--- kustomize_build_test.go
+++ kustomize_build_test.go
@@ -9,11 +9,19 @@ import (
 func TestAccKustomizeBuild_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6Providers,
+		/*ExternalProviders: map[string]resource.ExternalProvider{
+			"random": {
+				Source: "registry.terraform.io/hashicorp/random",
+			},
+		},*/
 		Steps: []resource.TestStep{
 			{
 				Config: `data "kustomize_build" "test" {
 						resources = ["github.com/hetznercloud/csi-driver/deploy/kubernetes"]
 					}`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.kustomize_build.test", "resources.0", "github.com/hetznercloud/csi-driver/deploy/kubernetes"),
+				),
 			},
 		},
 	})
